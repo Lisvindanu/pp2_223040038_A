c118d98f7a99a35a7eb0c1e4c4ac2436ece1f468
Lisvindanu <Lisvindanu015@gmail.com>
2024-09-29 15:08:05 +0700

    mengerjakan modul latihan pertemuan ke 2

---
+++ src/Pertemuan2/Latihan7/Main.java
@@ -0,0 +1,11 @@
+package Pertemuan2.Latihan7;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new MFrame();
+        });
+    }
+}
